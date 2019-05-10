5e0013137d8f32c92bc9c99d1a2c7c5bad22fe42
leander <18360183721@163.com>
2019-05-10 11:16:18 +0800

    依赖easyui

---
+++ app/src/main/java/com/pear/fartroom/controller/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pear.fartroom.controller.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pear.fartroom.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
